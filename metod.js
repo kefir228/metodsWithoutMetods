0164f6f9cf889e7278274034a1e051559c916e72
Ihor Karpov <igora75@gmail.com>
2024-05-10 17:04:16 +0300

    debug

--- metod.js
+++ metod.js
@@ -1,18 +1,24 @@
 // 1.
-// const Push = (arr , el) => {
-//     arr[arr.length] = el
+// const Push = (arr, ...el) => {
+//     for(elem of el){
+//         arr[arr.length] = elem
+//     }
 //     return arr
 //     }
-// console.log(Push([4,3,2], 1));
+// console.log(Push([4,3,2], 1, 2));
 // 2.
-// const Join = (arr) => {
+// const Join = (arr, divider) => {
 //     let str = ''
 //     for(let i = 0; i < arr.length; i++){
-//         str+=arr[i]
+//         if(i === arr.length - 1){
+//             str+=arr[i]
+//         }else{
+//             str += arr[i] + divider
+//         }
 //     }
 //     return str
 // }
-// console.log(Join(['oleg','21','football']));
+// console.log(Join(['oleg','21','football'], ' '));
 // 3.
 // const Pop = (arr) => {
 //     const lastElement = arr[arr.length - 1]
@@ -21,68 +27,71 @@
 // }
 // console.log(Pop([4,3,2,1]));
 // 4.
-// const Unshift = (arr, el) => {
-//     for(let i = arr.length; i > 0; i --){
-//         arr[i] = arr[i-1]
+// const Unshift = (arr, ...el) => {
+//     for (let i = arr.length + el.length - 1; i >= el.length; i--) {
+//         arr[i] = arr[i - el.length];
 //     }
-//     arr[0] = el
+//     for (let i = 0; i < el.length; i++) {
+//         arr[i] = el[i];
+//     }
 // return arr
 // }
-// console.log(Unshift([2,3,4],1));
+// console.log(Unshift([2,3,4],1,2));
 // 5.
 // const Shift = (arr) => {
 //     const firstEl = arr[(arr.length - arr.length)]
-//     arr.length = arr.length - 1
 //     return firstEl
 // }
 // console.log(Shift([1,2,3,4]));
 // 6.
-// const IndexOf = (arr , el) => {
-//     for(let i = 0; i < arr.length; i++){
+// const IndexOf = (arr , el, index) => {
+//     for(let i = index; i < arr.length; i++){
 //         if(arr[i]===el){
 //             return i
 //         }
 //     }
 //     return -1
 // }
-// console.log(IndexOf([1,2,3,4],3));
+// console.log(IndexOf([1,2,3,4], 3, 1));
 // 7.
-// const Includes = (arr , el) => {
-//     for(let i = 0; i < arr.length; i++){
+// const Includes = (arr , el, index) => {
+//     for(let i = index; i < arr.length; i++){
 //         if(arr[i]===el){
 //             return true
 //         }
 //     }
 //     return false
 // }
-// console.log(Includes([1,2,3,4],5));
+// console.log(Includes([1,2,3,4],1,2));
 // 8.
-// const Reverse = (arr) => {
-//     const revArr = []
+// const Reverse = (arr) => { недороблене
 //     const length = arr.length
 //     for(let i = 0; i < length; i ++){
-//         revArr[length - i - 1] = arr[i]
+//         arr[length - i - 1] = arr[i]
 //     }
-//     return revArr
+//     return [...arr]
 // }
 // console.log(Reverse([1,2,3,4]));
 // 9.
-// const Slice = (num) => {
+// const Slice = (num) => { недороблене
 //     const newArr = ['oleg', 2, 'moon', 'sun', 5]
 //     const arr = []
 //     for(let i = num; i < newArr.length; i++){
 //         arr[arr.length] = newArr[i] 
 //     }
+//     if(num < 0){
+//         arr[arr.length] = newArr[newArr.length - num]
+//     }
 //     return arr
 // }
-// console.log(Slice(1));
+// console.log(Slice(-1));
 // 10.
-// const LastIndexOf = (arr,el) => {
-//     for(let i = arr.length; i >= 0; i--){
+// const LastIndexOf = (arr,el,index ) => {
+//     for(let i = index; i < arr.length; i++){
 //         if(arr[i]===el){
 //             return i
 //         }
 //     }
 //     return -1
 // }
-// console.log(LastIndexOf([1,2,3,4,1,2],2));
+// console.log(LastIndexOf([1,2,3,4,1,2],1,1));
